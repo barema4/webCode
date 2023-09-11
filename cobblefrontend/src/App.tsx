6d5906a487f4e1cedd0f1e4fc9f995b2b07f11e7
sam rubarema <samrubarema6@gmail.com>
2023-09-11 09:31:46 +0300

    added footer and changed folders

--- cobblefrontend/src/App.tsx
+++ cobblefrontend/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './components/NavBar';
+import FooterBar from './components/FooterBar';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Navbar/>
+      <div className='middle-part'></div>
+      <FooterBar/>
     </div>
   );
 };
